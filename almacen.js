b93426330d26b57d1cb708ee232defa296f663ac
Nebulus10 <89684804+Nebulus10@users.noreply.github.com>
2022-05-22 16:12:15 +0200

    Update almacen.js

--- almacen.js
+++ almacen.js
@@ -19,9 +19,10 @@ var nodes = [
     var w = window.innerWidth;
     var h = window.innerHeight;
   
-    var svg = d3.select("body").append("svg").attr({"width":2000,"height":2000});
+    var svg = d3.select("body").append("svg").attr({"width":2000,"height":2000}); //It seems to be a problem with the resize of the svg element. Some elements were translated into further positions and I had to establish a 2000*2000 element
+                                                                                  // window.inner(Width/Height) should work but it doesn't
 
-var node = svg.selectAll("circle")
+var node = svg.selectAll("circle")  //The project asked for a "flie shape" but I couldn't achive to draw that shape, so I developed the project with black circles
     .data(nodes)
     .enter()
     .append("circle")
@@ -35,13 +36,13 @@ var node = svg.selectAll("circle")
 
         d3.select(this).style('fill', 'black').transition().attr("r", 0).duration(3000);
         
-        if(d.contador_mosca === 0){
+        if(d.contador_mosca === 0){ //conditions to know where the circle should go
             
-            d.bx = d.x
+            d.bx = d.x //Once we have the first coordinate, we save it for later
             d.by = d.y
             d.x = d.x1;
             d.y = d.y1;
-            d.contador_mosca = d.contador_mosca + 1;
+            d.contador_mosca = d.contador_mosca + 1; //this is how we now in which step we are
             d.r = 25;
 
         }
@@ -71,14 +72,14 @@ var node = svg.selectAll("circle")
 
         else if(d.contador_mosca === 4){
 
-            d3.select(this).style('fill', 'red').transition().attr("r", 0).duration(3000);
+            d3.select(this).style('fill', 'red').transition().attr("r", 0).duration(3000); //Animation of "death"
 
             d3.select(this).delay(6000).remove();
 
         }
 
         
-        d3.select(this).transition().attr("r", 25).duration(2000).attr("transform","translate("+d.x+","+d.y+")")
+        d3.select(this).transition().attr("r", 25).duration(2000).attr("transform","translate("+d.x+","+d.y+")") //This is how the shape is moved on the screen
        
         
     });
